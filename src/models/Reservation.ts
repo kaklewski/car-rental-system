6fcf070b173f137b68416ce5f9ffe87115a39738
Oskar Kąklewski <oskarkaklewski@gmail.com>
2026-03-04 21:45:59 +0100

    feat: Add reservation model

---
+++ src/models/Reservation.ts
@@ -0,0 +1,22 @@
+import type Car from './Car';
+
+class Reservation {
+    carType: Car;
+    startDate: Date;
+    days: number;
+    endDate: Date;
+
+    constructor(carType: Car, startDate: Date, days: number) {
+        if (days < 1) {
+            throw new Error('Reservation must be for at least 1 day');
+        }
+
+        this.carType = carType;
+        this.startDate = startDate;
+        this.days = days;
+
+        this.endDate = new Date(startDate.getTime() + days * 24 * 60 * 60 * 1000);
+    }
+}
+
+export default Reservation;
